6e1d22984e61ca1c878370252509108d06cde4e0
mccameg <meganmccarren@yahoo.com>
2023-02-05 19:46:49 -0500

    2.0

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
     public static void main(String[] args) {
 
     double purchasePrice = 100;
-    
+    double taxRate = .05;
+    double salesTaxAmount = 0;
+    salesTaxAmount = taxRate * purchasePrice; // calculate the sales tax amount  by multiplying .05 by the purchase price
+    System.out.println("The sales tax on a purchase of $" + purchasePrice + " is $" + salesTaxAmount); // outputs the purchase price and the tax dollar amount
 
     }
 }
